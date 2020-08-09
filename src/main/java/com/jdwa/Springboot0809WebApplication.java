ba7c01c4755812d25082ec863c7a0dd297f04d34
caozhou_zch <caozhou_zch@163.com>
2020-08-09 10:47:45 +0800

    init project

---
+++ src/main/java/com/jdwa/Springboot0809WebApplication.java
@@ -0,0 +1,13 @@
+package com.jdwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0809WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot0809WebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jdwa/Springboot0809WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot0809WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
