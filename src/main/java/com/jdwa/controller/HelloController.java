2839d4b94c5337617c4bd673308af258cec20053
caozhou_zch <caozhou_zch@163.com>
2020-08-09 17:43:06 +0800

    thmeleaf语法-集合(初次调试出错是因为ide自动补全大括号导致多了一个)

--- src/main/java/com/jdwa/controller/HelloController.java
+++ src/main/java/com/jdwa/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.jdwa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +14,8 @@ public class HelloController {
     //查出用户数据，再界面展示
     @RequestMapping("/success")
     public String success(Map<String,Object> map){
-        map.put("hello","你好！");
+        map.put("hello","<h1>你好！</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success";
     }
 }
