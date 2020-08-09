0c29b4a052c0f8d095807d5c926a37bddb320887
caozhou_zch <caozhou_zch@163.com>
2020-08-09 17:20:38 +0800

    简单的thmeleaf语法

--- src/main/java/com/jdwa/controller/HelloController.java
+++ src/main/java/com/jdwa/controller/HelloController.java
@@ -3,12 +3,17 @@ package com.jdwa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 
+    //查出用户数据，再界面展示
     @RequestMapping("/success")
-    public String success(){
+    public String success(Map<String,Object> map){
+        map.put("hello","你好！");
         return "success";
     }
 }
